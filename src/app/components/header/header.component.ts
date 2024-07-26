746531dd1cc2d25f2cfa10f176964c8860784a13
Grant Bentley <gbentley0618@comcast.net>
2024-07-26 11:11:25 -0500

    add button functionality to console.log "toggle"

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
+
+  toggleAddTask() {
+    console.log('toggle');
+  }
 
 }
